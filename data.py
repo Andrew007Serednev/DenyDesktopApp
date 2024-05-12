8fd5ef1ee39c974dceac368c8d9bcb0f7e506fa7
aasere13 <aasere13@mts.ru>
2024-05-12 16:27:02 +0300

    load, remove waybills jsons

--- data.py
+++ data.py
@@ -2,17 +2,24 @@ import json
 import pathlib
 
 
-class JsonData:
+class WaybillData:
+    def __init__(self):
+        self.directory = pathlib.Path('.\waybills')
 
     def get_waybill_list(self):
-        directory = pathlib.Path('.\waybills')
         pattern = '*.json'
         waybillist = []
-        for currentFile in directory.glob(pattern):
+        for currentFile in self.directory.glob(pattern):
             waybillist.append(currentFile.stem)
         print(waybillist)
         return waybillist
 
+    def remove_waybill_file(self, name):
+        json_file = pathlib.Path.joinpath(self.directory, name).with_suffix('.json')
+        json_file.unlink()
+
+
 # if __name__ == "__main__":
-#     jsonData = JsonData()
+#     jsonData = WaybillData()
 #     waybilllist = jsonData.get_waybill_list()
+#     jsonData.remove_waybill_file('12-06 36 5 4-4 705')
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QErrorMessage
 from Waybills import Ui_MainWindow
 import data
 
@@ -12,13 +12,27 @@ class Appl(QMainWindow):
         self.ui.setupUi(self)
 
         self.loadWayBills()
+        self.ui.waybilDeleteButton.clicked.connect(self.deleteWaybill)
 
     def loadWayBills(self):
-        jsonData = data.JsonData()
-        waybil_list = jsonData.get_waybill_list()
-        self.ui.waybillList.addItems(waybil_list)
+        waybills = data.WaybillData()
+        waybill_list = waybills.get_waybill_list()
+        self.ui.waybillList.addItems(waybill_list)
         self.ui.waybillList.setCurrentRow(0)
 
+    def deleteWaybill(self):
+        current_index = self.ui.waybillList.currentRow()
+        item = self.ui.waybillList.item(current_index)
+        if item is None:
+            return
+        question = QMessageBox.question(self, 'Удаление путевого листа',
+                                        'Вы точно хотите удалить выбранный путевой лист\n'
+                                        f'{item.text()}',
+                                        QMessageBox.Yes | QMessageBox.No)
+        if question == QMessageBox.Yes:
+            item = self.ui.waybillList.takeItem(current_index)
+            data.WaybillData().remove_waybill_file(item.text())
+            del item
 
 def app():
     app = QApplication(sys.argv)
