d12b91506418339682836a263b7384062f4b207a
aasere13 <aasere13@mts.ru>
2024-07-15 00:48:53 +0300

    New drivers class

--- windows/main.py
+++ windows/main.py
@@ -20,21 +20,21 @@ class MainWindow(QMainWindow):
 
         self.table_widget = self.main_window.tableWidget
         self.table_widget.setRowCount(7)
-        self.table_widget.setColumnCount(3)
+        self.table_widget.setColumnCount(4)
         self.table_widget.setHorizontalHeaderLabels(
-            ['Водитель', 'Автобус', 'Маршрут'])
+            ['Автобус', 'Маршрут', 'График', 'Водитель'])
         self.load_drivers_col()
         self.load_bus_col()
 
     def load_drivers_col(self):
         for row in range(self.table_widget.rowCount()):
             drivers_list = DriversList(self)
-            self.table_widget.setCellWidget(row, 0, drivers_list)
+            self.table_widget.setCellWidget(row, 3, drivers_list)
 
     def load_bus_col(self):
         for row in range(self.table_widget.rowCount()):
             bus_list = BusList(self)
-            self.table_widget.setCellWidget(row, 1, bus_list)
+            self.table_widget.setCellWidget(row, 0, bus_list)
 
     def open_new_driver(self):
         driver_dialog = DriverDialog()
